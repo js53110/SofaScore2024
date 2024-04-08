55ea558bbd65975a787a998a1b6ed78ad9173bc6
Akademija <akademija@MacBook-Pro-od-Akademija.local>
2024-04-08 13:04:29 +0200

    moved tableView to viewController

--- SofascoreAcademy/SofascoreAcademy/LeaguesView.swift
+++ SofascoreAcademy/SofascoreAcademy/LeaguesView.swift
@@ -11,80 +11,21 @@ import SofaAcademic
 
 class LeaguesView: BaseView {
         
-    private let tableView = UITableView()
     
-    override init() {
-        super.init()
-        
-        tableView.register(MatchViewCell.self, forCellReuseIdentifier: MatchViewCell.identifier)
-        tableView.register(LeagueInfoViewHeader.self, forHeaderFooterViewReuseIdentifier: LeagueInfoViewHeader.identifier)
-        
-        if #available(iOS 15.0, *) {
-            tableView.sectionHeaderTopPadding = 0
-        }
-    }
+    
     
     func update(){
-        tableView.dataSource = self
-        tableView.delegate = self
-        tableView.separatorStyle = .none
-        tableView.reloadData()
+        
     }
     
     override func addViews() {
-        addSubview(tableView)
     }
     
     override func setupConstraints() {
-        tableView.snp.makeConstraints {
-            $0.edges.equalToSuperview()
-        }
+        
     }
 }
 
-extension LeaguesView: UITableViewDataSource {
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        leaguesData.count
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        leaguesData[section].matches.count
-    }
-    
 
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if let cell = tableView.dequeueReusableCell(
-            withIdentifier: MatchViewCell.identifier,
-            for: indexPath) as? MatchViewCell {
-                let dataForRow = leaguesData[indexPath.section].matches[indexPath.row]
-                cell.update(data: dataForRow)
-                return cell
-        } else {
-            fatalError("Failed to equeue cell")
-        }
-    }
-    
-    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        if let headerView = tableView.dequeueReusableHeaderFooterView(
-            withIdentifier: LeagueInfoViewHeader.identifier) as? LeagueInfoViewHeader {
-                let sectionData = leaguesData[section]
-                headerView.update(
-                    countryName: sectionData.countryName,
-                    leagueName: sectionData.leagueName,
-                    leagueLogo: sectionData.leagueLogo)
-                return headerView
-        } else {
-            fatalError("Failed to dequeue header")
-        }
-    }
-}
-
-extension LeaguesView: UITableViewDelegate {
-    
-    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 56
-    }
-}
 
 
--- SofascoreAcademy/SofascoreAcademy/ViewController.swift
+++ SofascoreAcademy/SofascoreAcademy/ViewController.swift
@@ -3,13 +3,30 @@ import SnapKit
 import SofaAcademic
 
 class ViewController: UIViewController, BaseViewProtocol {
-    
-    private let leaguesView = LeaguesView()
+        
+    private let tableView = UITableView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+                
+        tableView.register(
+            MatchViewCell.self,
+            forCellReuseIdentifier: MatchViewCell.identifier
+        )
         
-        leaguesView.update()
+        tableView.register(
+            LeagueInfoViewHeader.self,
+            forHeaderFooterViewReuseIdentifier: LeagueInfoViewHeader.identifier
+        )
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.separatorStyle = .none
+        tableView.reloadData()
+        
+        if #available(iOS 15.0, *) {
+            tableView.sectionHeaderTopPadding = 0
+        }
                 
         addViews()
         styleViews()
@@ -18,7 +35,7 @@ class ViewController: UIViewController, BaseViewProtocol {
     }
     
     func addViews() {
-        view.addSubview(leaguesView)
+        view.addSubview(tableView)
     }
     
     func styleViews() {
@@ -26,12 +43,57 @@ class ViewController: UIViewController, BaseViewProtocol {
     }
     
     func setupConstraints() {
-        leaguesView.snp.makeConstraints() {
+        tableView.snp.makeConstraints() {
             $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
             $0.leading.trailing.bottom.equalToSuperview()
         }
     }
     
     func setupGestureRecognizers() {
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        leaguesData.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        leaguesData[section].matches.count
+    }
+    
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(
+            withIdentifier: MatchViewCell.identifier,
+            for: indexPath) as? MatchViewCell {
+                let dataForRow = leaguesData[indexPath.section].matches[indexPath.row]
+                cell.update(data: dataForRow)
+                return cell
+        } else {
+            fatalError("Failed to equeue cell")
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        if let headerView = tableView.dequeueReusableHeaderFooterView(
+            withIdentifier: LeagueInfoViewHeader.identifier) as? LeagueInfoViewHeader {
+                let sectionData = leaguesData[section]
+                headerView.update(
+                    countryName: sectionData.countryName,
+                    leagueName: sectionData.leagueName,
+                    leagueLogo: sectionData.leagueLogo)
+                return headerView
+        } else {
+            fatalError("Failed to dequeue header")
+        }
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 56
     }
 }
